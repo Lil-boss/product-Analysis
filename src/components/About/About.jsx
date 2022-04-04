b200d805c147144a8d33ee8fe2591b3aec0aabab
Lil-boss <rshaon09@gmail.com>
2022-04-05 01:08:12 +0600

    readme file updated

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            this is about
+        <div className='flex justify-center my-24'>
+            <img className='h-96' src="https://i.pinimg.com/originals/47/8c/cf/478ccf028523100af4bae44bc21b5ecd.jpg" alt="" />
         </div>
     );
 };
